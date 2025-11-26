09cefc6461c7d12a14c5263af4b151fa5f607288
marmora193 <mar_193@hotmail.com>
2025-11-26 12:55:00 +0100

    Corregimos la base de datos, y, por tanto, el create.sql, el models.py y la clase UsuarioService dentro de services.py conforme a la nueva actualización de la bd.

--- db/models/models.py
+++ db/models/models.py
@@ -5,40 +5,60 @@ from sqlalchemy.orm import (declarative_base, relationship)
 
 Base = declarative_base()
 
-class Cliente(Base):
-    __tablename__="cliente"
+class Usuario(Base):
+    __tablename__="usuario"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    nombre = Column(String(50), nullable = False)
-    apellidos = Column(String(50), nullable=False)
-    fecha_nac = Column(Date, nullable=False)
-    dni = Column(String(9), unique=True, nullable=False)
-    email = Column(String(100), nullable=False)
-    nacionalidad = Column(String(50), nullable=False)
-    telefono = Column(String(20), nullable=False)
-    direccion = Column(String(100), nullable=False)
+    nombre = Column(String(100), nullable = False)
+    apellidos = Column(String(100))
+    fecha_nac = Column(Date)
+    dni = Column(String(9), unique=True)
+    email = Column(String(100))
+    nacionalidad = Column(String(50))
+    telefono = Column(String(20))
+    direccion = Column(String(100))
     activo = Column(Boolean, default=True)
 
     #Relación
-    tasaciones = relationship("Tasacion", back_populates="cliente")
+    tasaciones = relationship("Tasacion", back_populates="usuario")
+    ventas = relationship("Venta", back_populates="usuario")
 
-class VentaOro(Base):
-    __tablename__ = "venta_oro"
+
+class Estado(Base):
+    __tablename__ = "estado"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    fecha = Column(Date, unique=True, nullable=False)
-    precio_kg = Column(Numeric(12, 2), nullable=False)
+    descripcion = Column(String(20), nullable=False, unique=True)
+
+    # Relación
+    ventas = relationship("Venta", back_populates="estado")
 
 class Tasacion(Base):
     __tablename__ = "tasacion"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    cliente_id = Column(Integer, ForeignKey("cliente.id"), nullable=False)
-    fecha_tasacion = Column(Date, nullable=False)
+    usuario_id = Column(Integer, ForeignKey("usuario.id"), nullable=False)
+    fecha = Column(Date, nullable=False)
     peso_gramos = Column(Numeric(10, 2), nullable=False)
-    precio_oro_kg_en_fecha = Column(Numeric(12, 2), nullable=False)
+    valor = Column(Numeric(12,2),nullable=False)
     importe = Column(Numeric(12, 2), default=0)
-    estado = Column(String(10), nullable=False)
 
     #Relación
-    cliente = relationship("Cliente", back_populates="tasaciones")
+    usuario = relationship("Usuario", back_populates="tasaciones")
+    ventas = relationship("Venta", back_populates="tasacion")
+
+class Venta(Base):
+    __tablename__ = "venta"
+
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    usuario_id = Column(Integer, ForeignKey("usuario.id"), nullable=False)
+    estado_id = Column(Integer, ForeignKey("estado.id"), nullable=False)
+    precio = Column(Numeric(12, 2), nullable=False)
+    id_tasacion = Column(Integer, ForeignKey("tasacion.id"), nullable=False)
+    gramos = Column(Numeric(10, 2), nullable=False)
+
+    # Relación
+    usuario = relationship("Usuario", back_populates="ventas")
+    estado = relationship("Estado", back_populates="ventas")
+    tasacion = relationship("Tasacion", back_populates="ventas")
--- services/services.py
+++ services/services.py
@@ -1,14 +1,20 @@
 from db.config import session
-from db.models.models import Cliente, VentaOro, Tasacion
+from db.models.models import Usuario, Venta, Tasacion
 from sqlalchemy import func
 from datetime import date, timedelta
 import random
 
 
-class ClienteService:
+class UsuarioService:
     @staticmethod
-    def crear_cliente(nombre, apellidos, fecha_nac, dni, email, nacionalidad, telefono, direccion):
-        cliente = Cliente(
+    def crear_usuario(nombre, apellidos, fecha_nac, dni, email, nacionalidad, telefono, direccion):
+
+        #Validamos para que el dni no esté duplicado
+        if UsuarioService.buscar_por_dni(dni):
+            return None
+
+
+        usuario = Usuario(
             nombre = nombre,
             apellidos = apellidos,
             fecha_nac = fecha_nac,
@@ -19,29 +25,45 @@ class ClienteService:
             direccion = direccion
         )
 
-        session.add(cliente)
+        session.add(usuario)
         session.commit()
-        return cliente
+        return usuario
 
     @staticmethod
-    def listar_clientes():
-        return session.query(Cliente).filter_by(activo=True).all()
+    def listar_usuarios():
+        return session.query(Usuario).filter_by(activo=True).all()
 
     @staticmethod
     def buscar_por_dni(dni):
-        return session.query(Cliente).filter_by(dni = dni).first()
+        return session.query(Usuario).filter_by(dni = dni).first()
 
     @staticmethod
-    def mayor_edad(cliente):
+    def mayor_edad(usuario):
+        #Evitamos que devuelva None para que no crashee la app
+        if usuario.fecha_nac is None:
+            return False
+
         hoy = date.today()
-        edad = hoy.year - cliente.fecha_nac.year - ((hoy.month, hoy.day) < (cliente.fecha_nac.month, cliente.fecha_nac.day))
+        edad = hoy.year - usuario.fecha_nac.year - ((hoy.month, hoy.day) < (usuario.fecha_nac.month, usuario.fecha_nac.day))
         return edad >= 18
 
     @staticmethod
     def dar_baja(dni):
-        cliente = ClienteService.buscar_por_dni(dni)
-        if cliente:
-            cliente.activo = False
+        usuario = UsuarioService.buscar_por_dni(dni)
+        if usuario:
+            usuario.activo = False
             session.commit()
-        return cliente
+        return usuario
 
+class VentaService:
+
+    @staticmethod
+    def get_precio_fecha(fecha:date):
+        "Devuelve el precio del oro para una fecha concreta. Si no existe devuelve NONE."
+        return session.query(Venta).filter_by(fecha=fecha).first()
+
+    @staticmethod
+    def generar_precio(fecha:date):
+        "Genera un precio aleatorio entre -3% y +3% a partir del precio del día anterior."
+
+        #Buscar el día anterior
